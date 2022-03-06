7a371d9966e4d48328dd1f4e1677d7c516288fd3
ALLANVILASBOAS <alan07vb@gmail.com>
2022-03-06 16:04:42 -0300

    add switch

---
+++ Sintaxe Básica em Javascript/Estruturas Condicionais/switch.js
@@ -0,0 +1,14 @@
+var jogador1 = 0;
+var jogador2 = 5;
+var placar;
+
+//função switch verificando case em variável 'placar';
+switch(placar){
+    case placar =jogador1 > jogador2:
+        console.log('Jogador1 ganhou!');
+        break;
+    case placar = jogador2 > jogador1:
+        console.log('jogador2 ganhou!');
+        break;
+        default: console.log('Empate!');
+}
